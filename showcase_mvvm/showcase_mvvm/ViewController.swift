8211cb463cbd4eaebd6a8e806f5f3695308684b3
Zaak <azaak@s0124671.ocn.ottogroup.com>
2019-06-20 14:36:47 +0200

    feat: created project

---
+++ showcase_mvvm/showcase_mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  showcase_mvvm
+//
+//  Created by Zaak, Alexander on 20.06.19.
+//  Copyright © 2019 Zaak, Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
