25b9ec38abb2ad4d9f554a318defc1ed7c690f18
Zaak <azaak@s0124671.ocn.ottogroup.com>
2019-06-22 20:11:22 +0200

    refactor: added android project and renamed ios project

---
+++ showcase_mvvm_ios/Pods/Target Support Files/Pods-showcase_mvvmUITests/Pods-showcase_mvvmUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_showcase_mvvmUITests : NSObject
+@end
+@implementation PodsDummy_Pods_showcase_mvvmUITests
+@end
---
+++ showcase_mvvm_ios/showcase_mvvm/common/ApiClient.swift
@@ -0,0 +1,52 @@
+//
+//  ApiClient.swift
+//  showcase_mvvm
+//
+//  Created by Zaak, Alexander on 21.06.19.
+//  Copyright © 2019 Zaak, Alexander. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxRetroSwift
+
+class ApiClient {
+    static var shared = ApiClient()
+
+    lazy var caller:RequestCaller = {
+        let config = URLSessionConfiguration.default
+        if #available(iOS 11.0, *) {
+            config.waitsForConnectivity = true
+        }
+        return RequestCaller(config: config)
+    }()
+
+    private init() {
+        RequestModel.defaults.baseUrl = "https://www.metaweather.com/api"
+    }
+
+    func fetchWeather(id:Int) -> Observable<Result<WeatherResult, ErrorModel>> {
+        let request:URLRequest = RequestModel(
+            httpMethod: .get,
+            path: "location/\(id)")
+            .asURLRequest()
+
+        return caller.call(request)
+    }
+
+    func fetchLocations() -> Observable<Result<[Location], ErrorModel>> {
+        let request:URLRequest = RequestModel(
+            httpMethod: .get,
+            path: "location/search",
+            query: ["lattlong":"53.5582447,9.647645"])
+            .asURLRequest()
+
+        return caller.call(request)
+    }
+
+    func fetchImage(name: String) -> Observable<Data> {
+        return URLSession.shared.rx
+            .response(request: URLRequest(url: URL(string: "https://www.metaweather.com/static/img/weather/png/64/\(name).png")!))
+            .map({$0.data})
+    }
+}
